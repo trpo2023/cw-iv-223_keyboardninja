f6b03bd0d6f45fd6130685b4d122d0c02270d684
Janya <ebelaysh@gmail.com>
2023-05-22 00:34:16 +0700

    added a button EN, an array of words in two languages

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 static GtkWidget *word_entry = NULL;
 static GtkWidget *start_button = NULL;
+static GtkWidget *label_word = NULL;
+static GtkWidget *button_language = NULL;
+gchar *language = "en"; // По умолчанию будет использевания английский язык
+gint correct_words = 0;
+gchar *current_word;
+// Массивы слов на русском и английском языках
+gchar *russian_words[] = {"кот", "собака", "дерево", "солнце", "компьютер", "автомобиль", "яблоко", "страна", "город", "дом"};
+gchar *english_words[] = {"cat", "dog", "tree", "sun", "computer", "car", "apple", "country", "city", "house"};
 
 
 static gboolean on_window_closed(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
@@ -9,13 +17,56 @@ static gboolean on_window_closed(GtkWidget *widget, GdkEvent *event, gpointer us
     return TRUE;
 }
 
-static void on_start_clicked() {
+// Функция для вывода случайного слова на экран
+static void set_word() {
+    gint index;
+    gint num_words;
+    gchar **words;
+    
+    if (strcmp(language, "ru") == 0) { // Если выбран русский язык, используем массив слов на русском языке
+        words = russian_words;
+        num_words = G_N_ELEMENTS(russian_words);
+    } else { // Если выбран английский язык, используем массив слов на английском языке
+        words = english_words;
+        num_words = G_N_ELEMENTS(english_words);
+    }
+    
+    // Генерируем случайный индекс слова
+    index = g_random_int_range(0, num_words);
+    
+    current_word = words[index];
+    gtk_label_set_text(GTK_LABEL(label_word), current_word);
+}
+void on_language_clicked() {
+    if (strcmp(language, "en") == 0) {
+        language = "ru";
+        gtk_button_set_label(GTK_BUTTON(button_language), "RU");
+    } else {
+        language = "en";
+        gtk_button_set_label(GTK_BUTTON(button_language), "EN");
+    }
+}
 
-    //gtk_widget_hide(button);
+static void on_start_clicked() {
+    correct_words = 0;
+    set_word();
     gtk_widget_set_sensitive(start_button, FALSE);
     gtk_widget_set_sensitive(word_entry, TRUE);
 }
-
+// Функция обработки события "activate" поля ввода слова
+void on_input_activate(GtkEntry *entry, gpointer user_data) {
+    gchar *text = gtk_entry_get_text(GTK_ENTRY(word_entry));
+    
+    if (strcmp(text, current_word) == 0) { // Если введенное слово совпадает с текущим словом
+        correct_words++;
+    }
+    
+    //total_words++; // Увеличиваем счетчик общего количества слов
+    
+    set_word(); // Генерируем новое слово
+    
+    gtk_entry_set_text(GTK_ENTRY(word_entry), ""); // Очищаем поле ввода
+}
 int main(int argc, char *argv[]) {
     GdkRGBA color;
 
@@ -26,19 +77,28 @@ int main(int argc, char *argv[]) {
     gtk_widget_override_background_color(window, GTK_STATE_NORMAL, &color);
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
     gtk_window_present(GTK_WINDOW(window));
-  
+    
+    label_word = gtk_label_new("-");
+    gtk_label_set_selectable(GTK_LABEL(label_word), TRUE);
+
     word_entry = gtk_entry_new();
     gtk_widget_set_sensitive(word_entry, FALSE);
+    g_signal_connect(word_entry, "activate", G_CALLBACK(on_input_activate), NULL);
  
     start_button = gtk_button_new_with_label("Start");
     g_signal_connect(start_button, "clicked", G_CALLBACK(on_start_clicked), NULL);
 
+    button_language = gtk_button_new_with_label("EN");
+    g_signal_connect(button_language, "clicked", G_CALLBACK(on_language_clicked), NULL);
+    
+
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
- 
+    
+    gtk_box_pack_start(GTK_BOX(vbox), label_word, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), word_entry, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), start_button, FALSE, FALSE, 0);
-   
+    gtk_box_pack_start(GTK_BOX(vbox), button_language, FALSE, FALSE, 0);
     gtk_widget_show_all(window);
     g_signal_connect(window, "destroy", G_CALLBACK(on_window_closed), NULL);
 
