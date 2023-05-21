043a8135f7e6415832a46371616115e3cb2f1ae3
Dmitriy Krivosheev <roflanoff@users.noreply.github.com>
2023-05-21 17:48:52 +0700

    Now main.c can open clear Window

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <gtk/gtk.h>
 
-int main()
+int main(int argc, char *argv[]) 
 {
-
+    gtk_init(&argc, &argv);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_show_all(window);
+    gtk_main();
     return 0;
 }
