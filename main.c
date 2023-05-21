51c1353d1b1d75b9d7d6a3e9c30b6fa2b350c70d
Dmitriy Krivosheev <roflanoff@users.noreply.github.com>
2023-05-21 18:06:44 +0700

    Now window closes normally

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <gtk/gtk.h>
 
-int main(int argc, char *argv[]) 
-{
+static gboolean on_delete_event(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
+    gtk_main_quit();
+    return TRUE;
+}
+
+int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(on_delete_event), NULL);
     gtk_widget_show_all(window);
     gtk_main();
     return 0;
