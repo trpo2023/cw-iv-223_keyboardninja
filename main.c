8acc71ed472da7fbcb3cf6e3d7abf1cc17422801
Dmitriy Krivosheev <roflanoff@users.noreply.github.com>
2023-05-22 01:14:55 +0700

    Add Correct Word

--- main.c
+++ main.c
@@ -4,7 +4,8 @@ static GtkWidget *word_entry = NULL;
 static GtkWidget *start_button = NULL;
 static GtkWidget *label_word = NULL;
 static GtkWidget *button_language = NULL;
-gchar *language = "en"; // По умолчанию будет использевания английский язык
+static GtkWidget *accuracy_label = NULL;
+gchar *language = "en"; // По умолчанию будет использоваться английский язык
 gint correct_words = 0;
 gchar *current_word;
 // Массивы слов на русском и английском языках
@@ -52,6 +53,7 @@ static void on_start_clicked() {
     set_word();
     gtk_widget_set_sensitive(start_button, FALSE);
     gtk_widget_set_sensitive(word_entry, TRUE);
+    //gtk_widget_hide(accuracy_label);
 }
 // Функция обработки события "activate" поля ввода слова
 void on_input_activate(GtkEntry *entry, gpointer user_data) {
@@ -60,11 +62,12 @@ void on_input_activate(GtkEntry *entry, gpointer user_data) {
     if (strcmp(text, current_word) == 0) { // Если введенное слово совпадает с текущим словом
         correct_words++;
     }
-    
-    //total_words++; // Увеличиваем счетчик общего количества слов
-    
+    char buffer[100];
+    sprintf(buffer, "Correct words: %d", correct_words);
+    gtk_label_set_text(GTK_LABEL(accuracy_label), buffer);
+    gtk_widget_show(accuracy_label);
+
     set_word(); // Генерируем новое слово
-    
     gtk_entry_set_text(GTK_ENTRY(word_entry), ""); // Очищаем поле ввода
 }
 int main(int argc, char *argv[]) {
@@ -75,7 +78,7 @@ int main(int argc, char *argv[]) {
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gdk_rgba_parse(&color, "rgb(181, 184, 177)"); // серый
     gtk_widget_override_background_color(window, GTK_STATE_NORMAL, &color);
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
     gtk_window_present(GTK_WINDOW(window));
     
     label_word = gtk_label_new("-");
@@ -85,18 +88,23 @@ int main(int argc, char *argv[]) {
     gtk_widget_set_sensitive(word_entry, FALSE);
     g_signal_connect(word_entry, "activate", G_CALLBACK(on_input_activate), NULL);
  
+    accuracy_label = gtk_label_new("Press Start ");
+    gtk_widget_hide(accuracy_label);
+
     start_button = gtk_button_new_with_label("Start");
     g_signal_connect(start_button, "clicked", G_CALLBACK(on_start_clicked), NULL);
 
     button_language = gtk_button_new_with_label("EN");
     g_signal_connect(button_language, "clicked", G_CALLBACK(on_language_clicked), NULL);
     
+    
 
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), vbox);
     
     gtk_box_pack_start(GTK_BOX(vbox), label_word, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), word_entry, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), accuracy_label, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), start_button, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), button_language, FALSE, FALSE, 0);
     gtk_widget_show_all(window);
