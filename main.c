8aa56701b56958cc47371d36f496ad9594adf7f1
Janya <ebelaysh@gmail.com>
2023-05-21 23:43:18 +0700

    corrected word_entry

--- main.c
+++ main.c
@@ -17,24 +17,22 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
     GtkWidget *word_entry = gtk_entry_new();
+   // g_signal_connect(word_entry, "activate", G_CALLBACK(on_input_activate), NULL);
+    
+    GtkWidget *start_button = gtk_button_new_with_label("Start");
+    g_signal_connect(start_button, "clicked", G_CALLBACK(on_start_clicked), word_entry);
 
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    GtkWidget *start_button = gtk_button_new_with_label("Start");
-    gtk_box_pack_start(GTK_BOX(vbox), start_button, FALSE, FALSE, 0);
-
-    g_signal_connect(start_button, "clicked", G_CALLBACK(on_start_clicked), word_entry);
-
-    g_signal_connect(window, "destroy", G_CALLBACK(on_window_closed), NULL);
-    g_signal_connect_swapped(window, "delete-event", G_CALLBACK(gtk_widget_destroy), window);
-    g_signal_connect(window, "key-press-event", G_CALLBACK(on_window_closed), NULL);
-
     gtk_widget_set_sensitive(word_entry, FALSE);
+    gtk_box_pack_start(GTK_BOX(vbox), start_button, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), word_entry, FALSE, FALSE, 0);
 
     gtk_widget_show_all(window);
+    g_signal_connect(window, "destroy", G_CALLBACK(on_window_closed), NULL);
 
     gtk_main();
 
