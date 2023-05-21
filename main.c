fe3444c05015bf86bb417e06e3e51acaf0a692d4
Dmitriy Krivosheev <roflanoff@users.noreply.github.com>
2023-05-21 21:31:16 +0700

    Add input line, and rename 1 funtion

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <gtk/gtk.h>
 
-static gboolean on_delete_event(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
+static gboolean on_window_closed(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
     gtk_main_quit();
     return TRUE;
 }
 
 int main(int argc, char *argv[]) {
-    gtk_init(&argc, &argv);
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(on_delete_event), NULL);
-    gtk_widget_show_all(window);
-    gtk_main();
-    return 0;
+
+  gtk_init(&argc, &argv);
+
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  GtkWidget *word_entry = gtk_entry_new();
+
+  gtk_container_add(GTK_CONTAINER(window), word_entry);
+
+  g_signal_connect(window, "destroy", G_CALLBACK(on_window_closed), NULL);
+  g_signal_connect_swapped(window, "delete-event", G_CALLBACK(gtk_widget_destroy), window);
+  g_signal_connect(window, "key-press-event", G_CALLBACK(on_window_closed), NULL);
+
+  gtk_widget_show_all(window);
+
+  gtk_main();
+
+  return 0;
 }
