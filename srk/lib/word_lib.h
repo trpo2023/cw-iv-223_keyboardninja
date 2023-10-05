73b9b87e29fc72ad83f8dcbb834120f5023b352d
Janya <ebelaysh@gmail.com>
2023-10-05 18:52:35 +0700

    Create structure

---
+++ srk/lib/word_lib.h
@@ -0,0 +1,10 @@
+#ifndef WORD_LIB_H
+#define WORD_LIB_H
+
+#define WORDS_NUM 110
+#define MAX_LENGTH 40
+
+extern const char WORDS_EN[WORDS_NUM][MAX_LENGTH];
+extern const char WORDS_RU[WORDS_NUM][MAX_LENGTH];
+
+#endif
